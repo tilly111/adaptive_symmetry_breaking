f3e7c55e909236b462def0de63b0f80f4e47239c
Till.Aust <till.aust@student.uni-luebeck.de>
2022-03-30 11:22:59 +0200

    somethings wrong

--- ARGoS_simulation/data_generation_scripts/ants_symmetry_breaking.sh
+++ ARGoS_simulation/data_generation_scripts/ants_symmetry_breaking.sh
@@ -67,73 +67,73 @@ else
             INITIAL_COMMUNICATION_RANGE=45
       fi
       EXP_NAME=experiment_cl_sym_break_cross_inhib_00_${j}_comrng_${INITIAL_COMMUNICATION_RANGE}_options_${n}_env_${ENVIRONMENT}
-    tmp_counter=$(( ${tmp_counter} + 1 ))
+      tmp_counter=$(( ${tmp_counter} + 1 ))
 
-    NUM_ROBOTS=50        # number of robots
-    QUORUM=-1            # Quorum to stop experiment NOT USED ATM
-    EXP_LENGTH=2400      #length of the experiment in secs
-    DATA_FREQUENCY=1     # frequency of saving the experiment data
+      NUM_ROBOTS=50        # number of robots
+      QUORUM=-1            # Quorum to stop experiment NOT USED ATM
+      EXP_LENGTH=2400      #length of the experiment in secs
+      DATA_FREQUENCY=1     # frequency of saving the experiment data
 
-    HRS=07 # hours the script takes
-    MIN=00 # min the script takes
+      HRS=07 # hours the script takes
+      MIN=00 # min the script takes
 
-    #path to main directory
-    EXP_FOLDER=${HOME}/Programs/adaptive_symmetry_breaking
+      #path to main directory
+      EXP_FOLDER=${HOME}/Programs/adaptive_symmetry_breaking
 
-    # full path to the compiled robot behaviour/loopfunction
-    BEHAVIOUR_FILE=${EXP_FOLDER}/build/behaviours/agent_stub
-    LOOPFUNCTION_FILE=${EXP_FOLDER}/build/loop_functions/libkilogrid_stub
+      # full path to the compiled robot behaviour/loopfunction
+      BEHAVIOUR_FILE=${EXP_FOLDER}/build/behaviours/agent_stub
+      LOOPFUNCTION_FILE=${EXP_FOLDER}/build/loop_functions/libkilogrid_stub
 
-    CONFIG_FILE=${EXP_FOLDER}/ARGoS_simulation/loop_functions/kilogrid_conf_files/${conf}
+      CONFIG_FILE=${EXP_FOLDER}/ARGoS_simulation/loop_functions/kilogrid_conf_files/${conf}
 
-    # path to template
-    EXP_TEMPLATE_SRC=${EXP_FOLDER}/ARGoS_simulation/experiment/3_op_template_kilogrid.argos
-    JOB_TEMPLATE_SRC=${EXP_FOLDER}/ARGoS_simulation/data_generation_scripts/runjob_template_master.sh
-    JOB_TEMPLATE_SRC_RUN=${EXP_FOLDER}/ARGoS_simulation/data_generation_scripts/runjob_template_run.sh
+      # path to template
+      EXP_TEMPLATE_SRC=${EXP_FOLDER}/ARGoS_simulation/experiment/3_op_template_kilogrid.argos
+      JOB_TEMPLATE_SRC=${EXP_FOLDER}/ARGoS_simulation/data_generation_scripts/runjob_template_master.sh
+      JOB_TEMPLATE_SRC_RUN=${EXP_FOLDER}/ARGoS_simulation/data_generation_scripts/runjob_template_run.sh
 
-    EXP_DIR=${EXP_FOLDER}/experiments_cluster/${EXP_NAME}
-    mkdir -p ${EXP_DIR}
+      EXP_DIR=${EXP_FOLDER}/experiments_cluster/${EXP_NAME}
+      mkdir -p ${EXP_DIR}
 
-    DATA_DIR=${EXP_FOLDER}/data_cluster/${EXP_NAME}
-    mkdir -p ${DATA_DIR}
+      DATA_DIR=${EXP_FOLDER}/data_cluster/${EXP_NAME}
+      mkdir -p ${DATA_DIR}
 
-    JOB_DIR=${EXP_FOLDER}/job_cluster
-    mkdir -p ${JOB_DIR}
+      JOB_DIR=${EXP_FOLDER}/job_cluster
+      mkdir -p ${JOB_DIR}
 
-    JOB_FILE=${JOB_DIR}/${EXP_NAME}.sh
-    JOB_NAME=${EXP_NAME}
-    # creating header for new script
-    sed -e "s|jobname|${JOB_NAME}|" \
-      -e "s|min|${MIN}|" \
-      -e "s|hrs|${HRS}|" \
-      -e "s|logfile|${JOB_NAME}|" \
-      ${JOB_TEMPLATE_SRC} >${JOB_FILE}
-    for i in $(seq 0 19); do
+      JOB_FILE=${JOB_DIR}/${EXP_NAME}.sh
+      JOB_NAME=${EXP_NAME}
+      # creating header for new script
+      sed -e "s|jobname|${JOB_NAME}|" \
+        -e "s|min|${MIN}|" \
+        -e "s|hrs|${HRS}|" \
+        -e "s|logfile|${JOB_NAME}|" \
+        ${JOB_TEMPLATE_SRC} >${JOB_FILE}
+      for i in $(seq 0 19); do
 
-      EXP_FILE=${EXP_DIR}/${EXP_NAME}_${i}.argos # full path to the experiment configuration file
-      DATA_FILE=${EXP_NAME}_${i}.txt # Full path to the data file
+          EXP_FILE=${EXP_DIR}/${EXP_NAME}_${i}.argos # full path to the experiment configuration file
+          DATA_FILE=${EXP_NAME}_${i}.txt # Full path to the data file
 
-      # it is very important to keep this order in order to not overwrite stuff
-      sed -e "s|exp_length|${EXP_LENGTH}|" \
-        -e "s|randomseed|$(($i * 124))|" \
-        -e "s|behaviourpath|${BEHAVIOUR_FILE}|" \
-        -e "s|loopfunctionpath|${LOOPFUNCTION_FILE}|" \
-        -e "s|configfilename|${CONFIG_FILE}|" \
-        -e "s|datafilename|${DATA_FILE}|" \
-        -e "s|num_robots|${NUM_ROBOTS}|" \
-        -e "s|initialcommitmentquality|${INITIAL_COMMITMENT_QUALITY}|" \
-        -e "s|initialcommitment|${INITIAL_COMMITMENT}|" \
-        -e "s|numberofoptions|${n}|" \
-        -e "s|initialcommunicationrange|${INITIAL_COMMUNICATION_RANGE}|" \
-        -e "s|maxcommunicationrange|${MAX_COMMUNICATION_RANGE}|" \
-        -e "s|dynamicenvname|${CONFIG_FILE}|" \
-        ${EXP_TEMPLATE_SRC} >${EXP_FILE}
+          # it is very important to keep this order in order to not overwrite stuff
+          sed -e "s|exp_length|${EXP_LENGTH}|" \
+            -e "s|randomseed|$(($i * 124))|" \
+            -e "s|behaviourpath|${BEHAVIOUR_FILE}|" \
+            -e "s|loopfunctionpath|${LOOPFUNCTION_FILE}|" \
+            -e "s|configfilename|${CONFIG_FILE}|" \
+            -e "s|datafilename|${DATA_FILE}|" \
+            -e "s|num_robots|${NUM_ROBOTS}|" \
+            -e "s|initialcommitmentquality|${INITIAL_COMMITMENT_QUALITY}|" \
+            -e "s|initialcommitment|${INITIAL_COMMITMENT}|" \
+            -e "s|numberofoptions|${n}|" \
+            -e "s|initialcommunicationrange|${INITIAL_COMMUNICATION_RANGE}|" \
+            -e "s|maxcommunicationrange|${MAX_COMMUNICATION_RANGE}|" \
+            -e "s|dynamicenvname|${CONFIG_FILE}|" \
+            ${EXP_TEMPLATE_SRC} >${EXP_FILE}
 
 
-        sed -e "s|argosfile|${EXP_FILE}|" \
-          -e "s|wheresave|${DATA_DIR}/|" \
-          -e "s|savefile|${DATA_FILE}|" \
-        ${JOB_TEMPLATE_SRC_RUN} >>${JOB_FILE}
+            sed -e "s|argosfile|${EXP_FILE}|" \
+              -e "s|wheresave|${DATA_DIR}/|" \
+              -e "s|savefile|${DATA_FILE}|" \
+            ${JOB_TEMPLATE_SRC_RUN} >>${JOB_FILE}
+        done
     done
-  done
 fi
